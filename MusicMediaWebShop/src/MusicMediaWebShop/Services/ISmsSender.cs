bed38d263e9521443ad633f15d051cbb2cc19b44
damducduy <damducduy@DESKTOP-4LK9MI9>
2017-04-11 14:28:41 +0700

    first

---
+++ MusicMediaWebShop/src/MusicMediaWebShop/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicMediaWebShop.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
